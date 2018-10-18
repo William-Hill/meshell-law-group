24969f70153d5ed4e0a2aa6930862f39ea449bde
William Hill <mjh2225@gmail.com>
2018-10-17 22:10:51 -0700

    removed graphql code

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,6 @@ import logo from '../img/logo.png'
 
 export default class IndexPage extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
-
     return (
       <React.Fragment>
           <section className="hero is-large has-bg-img">
@@ -105,30 +102,7 @@ export default class IndexPage extends React.Component {
       </div>
     </section>
 
-    </React.Fragment>)
+    </React.Fragment>
+    )
   }
 }
-
-export const pageQuery = graphql`
-  query IndexQuery {
-    allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] },
-      filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
-    ) {
-      edges {
-        node {
-          excerpt(pruneLength: 400)
-          id
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            templateKey
-            date(formatString: "MMMM DD, YYYY")
-          }
-        }
-      }
-    }
-  }
-`
