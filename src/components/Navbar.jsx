80a2c6f8dc3d811cc87113f73c639149ce980105
William Hill <mjh2225@gmail.com>
2018-11-19 21:57:14 -0800

    added hamburger menu for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,29 +2,41 @@ import React from 'react';
 import Link from 'gatsby-link';
 import logo from '../img/logo.png';
 
-const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img id="logo" src={logo} alt="Meshel_Law_Group_logo" style={{ width: '80%', maxHeight: '100%' }} />
-          </figure>
-        </Link>
+const Navbar = () => {
+  const handleClick = () => {
+    document.querySelector('.navbar-menu').classList.toggle('is-active');
+  };
+  return (
+    <nav className="navbar is-transparent">
+      <div className="container">
+        <div className="navbar-brand">
+          <Link to="/" className="navbar-item">
+            <figure className="image">
+              <img id="logo" src={logo} alt="Meshel_Law_Group_logo" style={{ width: '80%', maxHeight: '100%' }} />
+            </figure>
+          </Link>
+          <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" onClick={handleClick}>
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+          </a>
+        </div>
+        <div className="navbar-menu">
+          <div className="navbar-end">
+            <Link className="navbar-item" to="/about">
+              About Us
+            </Link>
+            <Link className="navbar-item" to="/practice">
+              Practice Areas
+            </Link>
+            <Link className="navbar-item" to="/contact">
+              Contact
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className="navbar-end">
-        <Link className="navbar-item" to="/about">
-          About Us
-        </Link>
-        <Link className="navbar-item" to="/practice">
-          Practice Areas
-        </Link>
-        <Link className="navbar-item" to="/contact">
-          Contact
-        </Link>
-      </div>
-    </div>
-  </nav>
-);
+    </nav>
+  )
+};
 
 export default Navbar;
