7062323e47a98a69b49f96733c01d27548b63600
William Hill <mjh2225@gmail.com>
2019-01-13 22:15:43 -0800

    added icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import logo from '../img/logo.png';
+import { FaEnvelope, FaPhoneSquare, FaMapMarker } from 'react-icons/fa';
+
 
 const Footer = () => (
   <footer className="footer" id="footer" style={{backgroundColor: '#f5f5f5', padding: '3rem 1.5rem 6rem'}}>
@@ -11,20 +13,20 @@ const Footer = () => (
         <p style={{ marginTop: '1rem' }}>Based in downtown Dallas, Texas, The Meshel Law Group, PLLC is a nationwide, full service immigration law firm, serving the global community.</p>
       </div>
       <div className="column">
-        <h3>Office Address</h3>
+
+        <h3><span style={{paddingRight: ".5em", verticalAlign: "sub"}}><FaMapMarker /></span> Office Address</h3>
         <p>
-          <i className="fa fa-map-marker" aria-hidden="true" />
           3106 Commerce Street
           <br />
           Dallas, Texas 75226
         </p>
-        <hr />
+        <hr style={{backgroundColor: '#dbdbdb',  border: 'none', display: 'block', height: '1px', margin: '1.5rem 0'}} />
         <p>
-          <i className="fa fa-phone-square" aria-hidden="true" />
+          <span style={{paddingRight: ".5em", verticalAlign: "sub"}}><FaPhoneSquare /></span>
           469.333.3008
         </p>
         <p>
-          <i className="fa fa-envelope" aria-hidden="true" />
+          <span style={{paddingRight: ".5em", verticalAlign: "sub"}}><FaEnvelope /></span>
           <a href="mailto:christy@meshellawgroup.com">christy@meshellawgroup.com</a>
         </p>
       </div>
