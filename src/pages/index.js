1eea045dabfce2fffca44a292790b711704d39b1
William Hill <mjh2225@gmail.com>
2019-01-13 21:42:53 -0800

    added sections to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,10 +11,13 @@ export default class IndexPage extends React.Component {
 
     const title = data.markdownRemark.frontmatter.title
     console.log("title:", title)
-
+    const practiceAreasImage = data.markdownRemark.frontmatter.practiceAreasImage;
+    const communityImage = data.markdownRemark.frontmatter.communityImage;
+    const caseEvaluationImage = data.markdownRemark.frontmatter.caseEvaluationImage;
+    const bioImage = data.markdownRemark.frontmatter.bioImage;
     const bannerStyle = {
       background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${ !!image.childImageSharp ? image.childImageSharp.fluid.src : image })center center`,
-
+      backgroundSize: "cover"
     }
 
     return (
@@ -22,31 +25,120 @@ export default class IndexPage extends React.Component {
         <section className="hero is-large has-bg-img" style={bannerStyle}>
             <div className="hero-body">
               <div className="container has-text-centered">
-                <h1 className="title has-text-light">
-                  Clear, Concise Immigration Council
-                </h1>
+                <h1 className="title has-text-light">{data.markdownRemark.frontmatter.heroHeader}</h1>
                 <a className="button is-link" href="contact">Schedule a Consultation</a>
               </div>
             </div>
-          </section>
+        </section>
+
+        <section className="section">
+          <div className="container has-text-centered">
+            <h2 className="subtitle">{ data.markdownRemark.frontmatter.subtitle }</h2>
+            <h1 className="title">{ data.markdownRemark.frontmatter.headerTitle }</h1>
+            <p dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}></p>
+          </div>
+
+          <div className="container has-text-centered">
+            <a className="button is-link" href="practice" style={{marginTop: "1rem"}}>Learn More</a>
+          </div>
+        </section>
+
+        <section className="section" style={{backgroundColor: "whitesmoke"}}>
+          <div className="columns is-centered">
+            <div className="column has-text-centered">
+              <a href="family-immigration">
+                <p className="title">Practice Areas</p>
+                <img src={ !!practiceAreasImage.childImageSharp ? practiceAreasImage.childImageSharp.fluid.src : practiceAreasImage} alt="Practice Areas" />
+              </a>
+              <p style={{marginTop: "1rem"}}>{data.markdownRemark.frontmatter.practiceAreasText}</p>
+            </div>
+            <div className="column has-text-centered">
+              <a href="community-workshop">
+                <p className="title">Community Workshops</p>
+                <img src={ !!communityImage.childImageSharp ? communityImage.childImageSharp.fluid.src : communityImage } alt="Community Workshops" />
+              </a>
+              <p style={{marginTop: "1rem"}}>{data.markdownRemark.frontmatter.communityText}</p>
+            </div>
+            <div className="column has-text-centered">
+              <a href="contact">
+                <p className="title">Case Evaluation</p>
+                <img src={!!caseEvaluationImage.childImageSharp ? caseEvaluationImage.childImageSharp.fluid.src : caseEvaluationImage} alt="Case Evaluation" />
+              </a>
+              <p style={{marginTop: "1rem"}}>{data.markdownRemark.frontmatter.caseEvaluationText}</p>
+            </div>
+          </div>
+        </section>
+
+        <section class="section">
+          <div class="container has-text-centered">
+            <article class="media">
+              <figure class="media-left" style={{flexBasis: "20%", marginRight: "2rem"}}>
+                <p class="image">
+                  <img class="is-rounded" src={!!bioImage.childImageSharp ? bioImage.childImageSharp.fluid.src : bioImage} alt="Christy White" />
+                </p>
+              </figure>
+              <div class="media-content" style={{marginTop: "1rem"}}>
+                <div class="content">
+                  <p>
+                    <strong>{data.markdownRemark.frontmatter.bioHeader}</strong>
+                    <br></br>
+                    {data.markdownRemark.frontmatter.bioIntro}
+                  </p>
+                  <p>{data.markdownRemark.frontmatter.bioBody}</p>
+                  <a className="button is-link" href="about">Learn More</a>
+                </div>
+              </div>
+            </article>
+          </div>
+        </section>
       </Layout>
     )
   }
 }
 
-// IndexPage.propTypes = {
-//   data: PropTypes.shape({
-//     allMarkdownRemark: PropTypes.shape({
-//       edges: PropTypes.array,
-//     }),
-//   }),
-// }
-
 export const pageQuery = graphql`
   query IndexQuery {
     markdownRemark(frontmatter: {templateKey: {eq: "index"}}) {
+      html
       frontmatter {
         title
+        heroHeader
+        subtitle
+        headerTitle
+        practiceAreasText
+        practiceAreasImage {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        communityText
+        communityImage {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        caseEvaluationText
+        caseEvaluationImage {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        bioHeader
+        bioIntro
+        bioBody
+        bioImage {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
         image {
           childImageSharp {
             fluid(maxWidth: 2048, quality: 100) {
--- src/pages/index.md
+++ src/pages/index.md
@@ -2,5 +2,38 @@
 templateKey: 'index'
 path: /
 title: Index page
+heroHeader: Clear, Concise Immigration Council
 image: /img/posters_banner_large.jpg
+subtitle: Welcome to
+headerTitle: The Meshel Law Group, PLLC
+
+practiceAreasImage: /img/practice-areas-ico.png
+practiceAreasText: Family members including children, parents, spouses, and fiancé(e)s may qualify for permanent residence based on their relationship to a U.S. citizen or lawful permanent resident.
+
+communityImage: /img/community-ico.png
+communityText: We will hold quarterly Know your rights/community workshop date, time, location and sign up sheet, we also post photos from previous workshops.
+
+caseEvaluationImage: /img/case-ico.png
+caseEvaluationText: Whatever your particular legal concerns may be, you can be confident that the we are here to help. If you would like to request a case evaluation, please email us or call our office at 469.333.3008
+
+bioImage: /img/christy-white.jpg
+bioHeader: Christy White
+bioIntro: Christy M. White practices immigration law exclusively. Ms. White is the co-founder and managing partner of The Meshel Law Group, PLLC.
+bioBody: Ms. White was born and raised in Canton, Mississippi, an impoverished and underprivileged city. Eager not to become a statistic of the environment, she excelled in academia and developed a passion for liberating others. Therefore, at the age of 10 she knew she wanted to become an attorney.
+
 ---
+
+**The Meshel Law Group, PLLC**, a nationwide full
+service immigration legal firm. Located in the heart of Dallas,
+we specialize in employment, family, humanitarian, and removal
+immigration relief.
+
+
+**Meshel Law Group (MLG)** stands for improving our
+  clients&#39; position in society. It does not matter if you are
+  applying for lawful permanent residence through an employer or
+  family based petition, if we are fighting to keep your immigration
+  status, or if we are protecting immigrants against deportation.
+  At MLG, we encourage and empower our clients to come out from the
+  shadows of unlawful status and strive for not only their legal
+  rights, but a better quality of life.
