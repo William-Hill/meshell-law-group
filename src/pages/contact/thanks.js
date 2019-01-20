0d15ab1bc4fe75efeb129f2dd4d27b082b64af41
William Hill <mjh2225@gmail.com>
2019-01-19 18:27:15 -0800

    removed timeout

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -9,11 +9,6 @@ export default () => (
             <div className="content">
                 <h1>Thank you for contacting The Meshel Law Group!</h1>
                 <p>We will respond to your inquiry shortly.</p>
-                {setTimeout(()=>{
-                    navigate('/')
-                    // Add your logic for the transition
-                  }, 5000)
-              }
             </div>
           </div>
           </section>
